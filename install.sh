7cc7378f614fac427c64f638b2cd25150a15de10
hed <hed854@users.noreply.github.com>
2015-04-09 01:35:45 +0200

    Installing themes

--- install.sh
+++ install.sh
@@ -2,13 +2,19 @@
 
 backup_dir="backup_dotfiles"
 
-echo "creating backup dir"
-mkdir ~/$backup_dir
-echo "backup current .vimrc"
+echo "backup old files"
+mkdir -p ~/$backup_dir
 mv ~/.vimrc ~/$backup_dir/.vimrc
+mv ~/.gitconfig ~/$backup_dir/.gitconfig
+
 echo "copying .vimrc"
 cp .vimrc ~/.vimrc
-echo "backup current .gitconfig"
-mv ~/.gitconfig ~/$backup_dir/.gitconfig
 echo "copying .gitconfig"
 cp .gitconfig ~/.gitconfig
+
+if [ ! -d ~/.vim ];then
+	echo "installing vim"
+	mkdir -p ~/.vim/{colors,autoload,bundle,syntax}
+	wget http://www.vim.org/scripts/download_script.php?src_id=16802 -O ~/.vim/colors/desertEx.vim
+	wget http://www.vim.org/scripts/download_script.php?src_id=12849 -O ~/.vim/colors/256-grayvim.vim
+fi
