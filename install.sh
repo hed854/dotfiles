0bec30f8797de0c8a63f6d2e2f05514d1237e3a0
hed <hed854@users.noreply.github.com>
2015-09-16 01:31:32 +0200

    Better install script (using pathogen)

--- install.sh
+++ install.sh
@@ -1,20 +1,53 @@
 #!/bin/bash
 
-backup_dir="backup_dotfiles"
+if [ -z $1 ];then
+	echo "You must specify your home dir"
+	exit 1
+else
+	home_dir=$1
+fi
+backup_dir="backup/dotfiles"
+vim_dir="$home_dir/.vim"
 
-echo "backup old files"
+git_install()
+{
+	install_dir=$(echo $1 | cut -d'/' -f5-)
+	if [ -d $install_dir ]; then
+ 		echo "Installing $install_dir"
+ 		cd $vim_dir/bundle
+ 		git clone $1
+	else
+		echo "  - $install_dir... OK!"
+	fi
+}
+
+echo "Backup old files"
 mkdir -p ~/$backup_dir
 mv ~/.vimrc ~/$backup_dir/.vimrc
 mv ~/.gitconfig ~/$backup_dir/.gitconfig
 
-echo "copying .vimrc"
+echo "Copying .vimrc"
 cp .vimrc ~/.vimrc
-echo "copying .gitconfig"
+echo "Copying .gitconfig"
 cp .gitconfig ~/.gitconfig
 
-if [ ! -d ~/.vim ];then
-	echo "installing vim"
-	mkdir -p ~/.vim/{colors,autoload,bundle,syntax}
-	wget http://www.vim.org/scripts/download_script.php?src_id=16802 -O ~/.vim/colors/desertEx.vim
-	wget http://www.vim.org/scripts/download_script.php?src_id=12849 -O ~/.vim/colors/256-grayvim.vim
+
+if [ ! -d $vim_dir ];then
+	echo "Creating vim basic dirs"
+	mkdir -p $vim_dir/{autoload,bundle}
+else
+	echo "Vim basic dirs... OK!"
 fi
+if [ ! -e "$vim_dir/autoload/pathogen.vim" ]; then
+	echo "Installing Pathogen"
+	cd $vim_dir/autoload
+	wget https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+else
+	echo "Pathogen... OK!"
+fi
+
+echo "Installing plugins:"
+git_install "https://github.com/scrooloose/nerdtree"
+git_install "https://github.com/itchyny/lightline.vim"
+git_install "https://github.com/vim-scripts/desertEx"
+git_install "https://github.com/vim-scripts/256-grayvim"
