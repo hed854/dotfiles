d183e7efafb913ff438e219db19f4005056f2b1d
hed854 <hed854@users.noreply.github.com>
2022-01-16 13:58:12 +0100

    remove keyboard-related install functions

--- install.sh
+++ install.sh
@@ -10,18 +10,6 @@ else
 fi
 echo "Set home dir: $home_dir"
 
-# Parameter: keyboard type (optional)
-if [ -z $2 ]; then
-	keyboard_type="iso_tkl"
-else
-	case $2 in
-		"ansi") keyboard_type="ansi" ;;
-		"iso_tkl") keyboard_type="iso_tkl" ;;
-		*) echo "Invalid keyboard type"; exit 1 ;;
-	esac
-fi
-echo "Set keyboard type: $keyboard_type"
-
 backup_dir="backup/dotfiles"
 vim_dir="$home_dir/.vim"
 
@@ -114,34 +102,6 @@ git_install()
 	cp .gitconfig $home_dir/.gitconfig
 }
 
-generate_xmodmap()
-{
-	xmodmap_file=$home_dir/xmodmap.conf
-	# keycode: run `showkey` in a virtual console
-	# keysym: http://wiki.linuxquestions.org/wiki/List_of_Keysyms_Recognised_by_Xmodmap
-	# debug mapping: xmodmap -pke
-	MAP_ANSI=(
-		"keycode 100 = less NoSymbol"
-		"keycode 101 = greater NoSymbol"
-	)
-	MAP_ISO_TKL=(
-		"keycode 24 = a A NoSymbol NoSymbol aring Aring"
-	)
-	case $1 in
-		"ansi") map=$MAP_ANSI ;;
-		"iso_tkl") map=$MAP_ISO_TKL ;;
-	esac
-
-	echo "" > $xmodmap_file
-	for i in "${map[@]}"; do
-		echo "$i" >> $xmodmap_file
-
-	done
-	echo "Generated $xmodmap_file, apply manually (for now)"
-
-}
-
-generate_xmodmap $keyboard_type
 backup
 vim_install
 bashrc_install
